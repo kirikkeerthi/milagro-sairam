749f050e4162ae6d103ad7546b2ed4cd5b58d535
kirikkeerthi <sce22ee021@sairamtap.edu.in>
2025-03-31 10:14:46 +0530

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen text-white bg-gray-900">
-      <h1 className="text-4xl font-bold">🚀 Milagro Server is Online!</h1>
-    </main>
-  );
-}
